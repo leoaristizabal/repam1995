baa82d44fa8355f0fb64e1c3ce2602e62327624b
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-09-27 14:40:01 -0400

    change carrusel logic and add router to videobg and paleta bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Video from '../assets/videobg11.mp4';
 import Buscador from './Buscador';
 import HomeCarrusel from './HomeCarrusel';
 
@@ -7,7 +6,7 @@ const Home = () => {
   return (
     <div>
       <section className='relative top-0 h-[110vh]' data-aos='fade-down' data-aos-delay="300" data-aos-duration='3000' > {/** */}
-        <video className='absolute z-0 h-full w-full object-cover' src={Video} autoPlay loop muted></video>
+        <video className='absolute z-0 h-full w-full object-cover' src='/videobg11.mp4' autoPlay loop muted></video>
         <div className='flex flex-col justify-center items-center relative z-10 lg:h-full h-screen max-w-[1320px] px-6 xl:pt-0 pt-16 mx-auto '>
           <img src="./logoblanco.svg" className=' md:w-45 xl:max-w-[280px] max-w-[200px]' alt="" />
           <span className='lg:text-xl text-sm text-white text-center py-2 pb-8 px-8 font-bold relative'>
@@ -16,7 +15,7 @@ const Home = () => {
         {/*}  <p className='text-black text-center text-2xl my-8'>Mira nuestros productos mas vendidos!</p>*/}
           <Buscador/>
           <div className="text-white font-bold lg:text-1xl pt-8">Estamos contigo en:</div>
-          <img src="./heartharrow.png" className='lg:block w-32 hidden lg:my-2 -ml-9' alt="" />
+          <img src="/heartharrow.png" className='lg:block w-32 hidden lg:my-2 -ml-9' alt="" />
         </div>
       <HomeCarrusel/>
       </section>
--- src/components/HomeCarrusel.jsx
+++ src/components/HomeCarrusel.jsx
@@ -8,73 +8,88 @@ import { TbBeach } from 'react-icons/tb';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-
 const responsives = {
-    superLargeDesktop: {
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5,
-    },
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 4,
-    },
-    tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 4,
-    },
-    mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1,
-    },
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 4,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
 };
 
 const TiposAventuras = [
-    { title: "Playa", icon: <TbBeach /> },
-    { title: "Deportes", icon: <GiTennisRacket /> },
-    { title: "Aventuras", icon: <LiaWarehouseSolid /> },
-    { title: "Road", icon: <FaBiking /> },
-    { title: "Sol", icon: <FaSun /> },
-    { title: "Descubrir", icon: <RiPlanetLine /> },
+  { title: "Playa", icon: <TbBeach /> },
+  { title: "Deportes", icon: <GiTennisRacket /> },
+  { title: "Aventuras", icon: <LiaWarehouseSolid /> },
+  { title: "Road", icon: <FaBiking /> },
+  { title: "Sol", icon: <FaSun /> },
+  { title: "Descubrir", icon: <RiPlanetLine /> },
 ];
 
 const HomeCarrusel = () => {
-    const [currentSlide, setCurrentSlide] = useState(-1);
-    return (
-        <section className='bg-[#f3f8f6] bg-[url(./paletabg.svg)]'>
-            <div className='relative z-10 max-w-[1320px] px-6 mx-auto xl:-mt-28 -mt-12'>
-                <Carousel responsive={responsives} infinite autoPlay={true} itemClass="px-2">
-                    {TiposAventuras.map((item, index) => (
-                        <div key={index} className="group">
-                            <div className={`cursor-pointer p-8 rounded-lg flex flex-col justify-center items-start gap-4 ${currentSlide % TiposAventuras.length === index 
-                                ? 'bg-blue1 bg-opacity-40 text-white' 
-                                : "bg-white text-blue1 group-hover:bg-blue1 group-hover:bg-opacity-40 group-hover:text-white"}`}>
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-                                <p className={`font-bold text-lg ${currentSlide % TiposAventuras.length === index
-                                    ? 'text-white'
-                                    : 'text-blue1 group-hover:text-white'
-                                    }`}>{item.title}
-                                </p>
+  const handleAfterChange = (index) => {
+    setCurrentSlide(index);
+  };
 
+  return (
+    <section className='bg-[#f3f8f6] bg-[url(/paletabg.svg)]'>
+      <div className='relative z-10 max-w-[1320px] px-6 mx-auto xl:-mt-28 -mt-12'>
+        <Carousel 
+          responsive={responsives} 
+          infinite 
+          autoPlay={true} 
+          itemClass="px-2"
+          afterChange={handleAfterChange}  // Usa afterChange para actualizar el slide actual
+        >
+          {TiposAventuras.map((item, index) => (
+            <div key={index} className="group">
+              <div className={`cursor-pointer p-8 rounded-lg flex flex-col justify-center items-start gap-4 ${
+                currentSlide % TiposAventuras.length === index 
+                  ? 'bg-blue1 bg-opacity-40 text-white' 
+                  : 'bg-white text-blue1 group-hover:bg-blue1 group-hover:bg-opacity-40 group-hover:text-white'
+              }`}>
+                <p className={`font-bold text-lg ${
+                  currentSlide % TiposAventuras.length === index
+                    ? 'text-white'
+                    : 'text-blue1 group-hover:text-white'
+                }`}>
+                  {item.title}
+                </p>
 
-                                <span className={`text-6xl ${currentSlide % TiposAventuras.length === index
-                                    ? 'text-white'
-                                    : 'text-blue1 group-hover:text-white'
-                                    }`}>{item.icon}
-                                </span>
+                <span className={`text-6xl ${
+                  currentSlide % TiposAventuras.length === index
+                    ? 'text-white'
+                    : 'text-blue1 group-hover:text-white'
+                }`}>
+                  {item.icon}
+                </span>
 
-
-                                <p className={`${currentSlide % TiposAventuras.length === index
-                                    ? 'text-white'
-                                    : 'text-blue1 group-hover:text-white'}`} >Lorem ipsum dolor sit, amet consectetur 
-                                </p>
-
-                            </div>
-                        </div>
-                    ))}
-                </Carousel>
+                <p className={`${
+                  currentSlide % TiposAventuras.length === index
+                    ? 'text-white'
+                    : 'text-blue1 group-hover:text-white'
+                }`}>
+                  Lorem ipsum dolor sit, amet consectetur
+                </p>
+              </div>
             </div>
-        </section>
-    );
+          ))}
+        </Carousel>
+      </div>
+    </section>
+  );
 };
 
 export default HomeCarrusel;
